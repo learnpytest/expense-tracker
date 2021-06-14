db04968bbe468a7166d4f1ce581835a48bed7fda
RachelChen <iceage91@gmail.com>
2021-06-14 19:20:52 +0800

    feat: add edit function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -8,15 +8,21 @@ router.get('/new', (req, res) => {
   return res.send('This is records page')
 })
 
+// 編輯一筆資料
 router.get('/:id/edit', async (req, res) => {
   const { id } = req.params
   const record = await Record.findById({ _id: id }).lean()
   return res.render('edit', { record })
 })
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  await Record.findOneAndUpdate({ "_id": id }, { $set: req.body })
+  return res.redirect('/')
 })
+
+// 編輯一筆資料
+
 router.post('/', (req, res) => {
 
 })
