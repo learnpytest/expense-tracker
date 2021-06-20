493ac37be54423a5d75eedfdffd1f8fb5ff83656
RachelChen <iceage91@gmail.com>
2021-06-20 21:43:56 +0800

    modify mongoose connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
+const MONGODB_URI = 'mongodb+srv://root:root@cluster0.jftyp.mongodb.net/expensetracker?retryWrites=true&w=majority'
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
 const db = mongoose.connection
