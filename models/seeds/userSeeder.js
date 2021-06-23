b5bc93d20b9e62d0dd30b833325e98e880ad5700
RachelChen <iceage91@gmail.com>
2021-06-23 18:53:28 +0800

    feat: add bcrypt

--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -1,8 +1,13 @@
 const User = require('../User')
 const db = require('../../config/mongoose')
 const { user } = require('../../expense.json').results
+const bcrypt = require('bcryptjs')
 
-db.once('open', () => {
+// use bcrypt hash password for users
+db.once('open', async () => {
+  for (char of user) {
+    char.password = await bcrypt.genSalt(10).then(salt => bcrypt.hash(char.password, salt))
+  }
   User.insertMany(user, (err) => {
     if (err) return console.log(err, 'user seeder is not working')
     console.log('user seeder done')
